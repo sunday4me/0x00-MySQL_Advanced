f8d0da666db569ab6fe9f160dc0f56d7fed6c209
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:18:26 +0100

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module - 101-students
+"""
 
+
+def top_students(mongo_collection):
+    """
+    Use the aggregation framework to calculate
+    the average score for each student
+    """
+    pipeline = [
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ]
+    return mongo_collection.aggregate(pipeline)
