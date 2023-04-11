dda1ba8cb4d20f4442efe17ead0b387e589a8fbb
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:20:34 +0100

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module - school_by_topics
+"""
 
+
+def schools_by_topic(mongo_collection, topic):
+    """find document in collection"""
+    collection = mongo_collection.find({"topics": topic})
+    return collection
