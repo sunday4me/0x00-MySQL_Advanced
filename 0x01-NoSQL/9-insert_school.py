5927272fac54407ef55b52e510fdf6ffa67772f2
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:28:30 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module - insert_school
+"""
 
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts new document to a collection"""
+    collection = mongo_collection.insert_one(kwargs)
+    return collection.inserted_id
