3eaf5a8b436b18ab34ffc502b855c97d1807344f
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:27:50 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module - all
+"""
 
+
+def list_all(mongo_collection):
+    """lists all documents collection"""
+    collection = list(mongo_collection.find({}))
+    return collection
