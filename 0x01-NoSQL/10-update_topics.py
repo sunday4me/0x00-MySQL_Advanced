7379eb5d232c847564f51de65f08a8aa75e0869d
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:16:41 +0100

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module - update_topics
+"""
 
+
+def update_topics(mongo_collection, name, topics):
+    """updaten document in collection"""
+    collection = mongo_collection.update_many({"name": name},
+                                             {"$set": {"topics": topics}})
