a9ad87e7992c772cff7ed68d529f8340a8b9e6e7
Sunday  Jimoh <sunday4me@gmail.com>
2023-04-11 17:19:14 +0100

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,31 @@
+#!/usr/bin/env python3
+""" 102-log_stats """
+from pymongo import MongoClient
 
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    count_logs = collection.count_documents({})
+    print("{} logs".format(count_logs))
+
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    count_status = collection.count_documents({"path": "/status"})
+    print("{} status check".format(count_status))
+
+    pipeline = [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+    ]
+    results = collection.aggregate(pipeline)
+    print('IPs:')
+    for result in results:
+        print("\t{}: {}".format(result["_id"], result["count"]))
